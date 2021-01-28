1804e134dc38614fce65b4eb9e20da875f9450d0
Unknown <vadimkor@yandex.ru>
2021-01-28 20:00:34 +0300

    init

---
+++ granum.js
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', e => {
+  console.log('granum.js started')
+
+  // init toggler state
+  document.querySelectorAll('.toggle').forEach(a => {
+    const n = document.querySelector(a.dataset.nodes || a.hash)
+    if (n) a.classList[(n.classList.contains((a.dataset.set || 'show').split(/\s+/)[0])) ? 'add' : 'remove']('act')
+  })
+
+  
+  // align table cells
+  document.querySelectorAll('table').forEach(n => {
+    n.className.match(/\b[lcr]\d\d?\b/g)
+      ?.forEach(c => {
+        n.querySelectorAll('tr>*:nth-child(' + c.substr(1) + ')')
+        .forEach(td => td.classList.add(c.substr(0, 1)))
+      })
+  })
+  
+  // init form inputs
+  document.querySelectorAll('form[data-get]').forEach(n => {
+    n.querySelectorAll('[name]').forEach(m => ('get' in m.dataset) ? null : m.dataset.get = m.name)
+  })
+  
+  // init input values
+  document.querySelectorAll('[name][data-get]').forEach(n => {
+    const nm = n.dataset.get || n.name
+    const m = location.href.match(new RegExp('(\\?|&)' + nm + '=(.*?)($|&|#)'))
+    if (m) {
+      const v = decodeURIComponent(m[2].replace(/\+/g, ' '))
+      if (n.type == 'checkbox') n.checked = (v && v !== '0')
+      else if (n.type == 'radio') n.checked = (v && n.value === v)
+      else n.value = v
+    }
+  })
+})
+
+document.addEventListener('click', e => {
+  const a = e.target.closest('a')
+  
+  if (a && a.classList.contains('dialog')) {
+    // prompt link
+    const p = a.dataset.prompt
+    if (p) {
+      e.preventDefault()
+      const v = prompt(a.title || a.textContent, a.dataset.default || '')
+      if (v != null) location.href = a.href.replace(p, v)
+    }
+    // confirm link
+    else if (!confirm(a.title || a.textContent)) e.preventDefault()
+  }
+
+  // toggle
+  if (a && a.hash && a.classList.contains('toggle')) {
+    e.preventDefault()
+    const m = document.querySelectorAll(a.dataset.nodes || a.hash)
+    const c = (a.dataset.set || 'show').split(/\s+/)
+    const on = !m[0].classList.contains(c[0])
+    m.forEach(n => {
+      c.forEach(k => n.classList[on ? 'add' : 'remove'](k))
+      a.classList[on ? 'add' : 'remove']('act')
+    })
+    if (location.hash && c[0] == 'show') location.hash = '#cancel'
+  }
+  
+  // close modal
+  if (e.target.classList.contains('modal')) location.hash = '#cancel'
+})
+
+document.addEventListener('input', e => {
+  // check all boxes
+  const g = e.target.dataset.group
+  if (g) document.querySelectorAll('.' + g + '[type="checkbox"]')
+    .forEach(n => n.checked = e.target.checked)
+})
+
+// close modals
+document.addEventListener('keydown', e => {
+  if (e.key == 'Escape') location.hash = '#cancel'
+})
+
+// resize window
+window.addEventListener('resize', e => {
+  const m = (window.innerWidth <= 900)
+  if (document.body._m == null || document.body._m != m) {
+    document.body._m = m
+    document.querySelectorAll('[data-mobile], [data-desktop]')
+      .forEach(n => n.className = n.dataset[m ? 'mobile' : 'desktop'] || '')
+  }
+})
+
+window.dispatchEvent(new Event('resize'))
+
