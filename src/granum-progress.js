8924b8832f5d0bce019ac9605ecf0bf0c250f5b5
vvvkor <vvvkor@gmail.com>
2023-11-17 12:35:17 +0300

    test addons: progress

--- src/granum-progress.js
+++ src/granum-progress.js
@@ -6,25 +6,34 @@
     let n = document.querySelector('.progress')
     if (!evt.detail) {
       uploads = []
-      if (n) {
-        n.innerHTML = ''
-        n.classList.add('hide')
-      }
+      if (n) n.remove()
       return
     }
     const fn = evt.detail.name
     const e = evt.detail.event
-    const data = [fn, e.loaded, e.total]
+    const data = [fn, e?.loaded, e?.total]
     const i = uploads.findIndex(f => f[0] == fn)
-    if (i != -1) uploads[i] = data
+    if (i != -1 && !e) uploads = uploads.filter(u => u[0] != fn)
+    else if (i != -1) uploads[i] = data
     else uploads.push(data)
     if (!n) {
       n = document.createElement('div')
       n.classList.add('progress')
+      n.innerHTML = `<table><thead><tr><th><a class="icon-upload"></a></th><th></th><th class="r"><a class="icon-close" href="#close"></th></tr></thead><tbody></tbody></table>`
       document.body.append(n)
     }
-    if (n) n.innerHTML = `<table><thead><tr><th><a class="icon-upload"></a></th><th></th><th class="r"><a class="icon-close" href="#close" onclick="document.dispatchEvent(new Event('granum-progress'))"></th></tr></thead><tbody>${uploads.map(f => `<tr><td>${f[0]}</td><td title="${f[1]} / ${f[2]}"><progress value="${f[1]}" max="${f[2]}"></td><td class="${f[1] == f[2] ? 'icon-ok text-y' : 'icon-time text-n'}"></td></tr>`).join('')}</tbody></table>`
+    const b = n.querySelector('tbody')
+    if (b) b.innerHTML = `${uploads.map(f => `<tr><td>${f[0]}</td><td><progress value="${f[1]}" max="${f[2]}"></td><td class="${f[1] == f[2] ? 'icon-ok text-y' : 'icon-time text-n'}"></td></tr>`).join('')}</tbody>`
     n.classList.toggle('hide', uploads.length == 0)
+  })
+  
+  document.addEventListener('click', e => {
+    const a = e.target.closest('.progress a[href="#close"]')
+    const x = e.target.closest('.progress .icon-ok, .progress .icon-time')
+    if (a) {
+      e.preventDefault()
+      document.dispatchEvent(new Event('granum-progress'))
+    }
   })
 
 })()
