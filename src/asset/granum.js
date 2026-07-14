d3f2a92cacf44235f69b11502fd1a2fc5202a2e3
vvvkor <38428286+vvvkor@users.noreply.github.com>
2026-07-14 11:14:54 +0300

    esc

--- src/asset/granum.js
+++ src/asset/granum.js
@@ -241,7 +241,7 @@ window.addEventListener('close', e => (e.target.id == location.hash.slice(1)) ? 
 // escape key // legacy
 document.addEventListener('keydown', e => {
   // close modals and popups (no need for modern dialog/popover, works natively)
-  if (e.key == 'Escape') {
+  if (e.key == 'Escape' && location.hash) {
     location.hash = '#escape'
     document.querySelectorAll('details.pop').forEach(n => n.removeAttribute('open'))
   }
